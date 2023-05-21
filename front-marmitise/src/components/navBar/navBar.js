81351995acbd700de8cd5d25ed6add952ac18788
Nicolas Germani <nicolas.germani.ndv@gmail.com>
2023-05-21 02:52:26 +0200

    la navBar est responsive

--- front-marmitise/src/components/navBar/navBar.js
+++ front-marmitise/src/components/navBar/navBar.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import { NavLink, useNavigate } from "react-router-dom";
 
 import logo from '../../images/logo.png';
@@ -6,10 +6,12 @@ import './navBar.css';
 
 export default function NavBar(props){
 
+    var isNavOpen = false;
+    const navigate = useNavigate();
+
     useEffect(() => {
         props.changeIsLogged();
     })
-    const navigate = useNavigate();
 
     function logOut() {
         localStorage.setItem('user', '');
@@ -26,44 +28,49 @@ export default function NavBar(props){
         props.changeStateBtnLogin(true)
         navigate('/login');
     }
-    
 
-    const navBarLogIn = (
+    function toggleNav() {
+        isNavOpen = !isNavOpen;
+        console.log(isNavOpen);
+        var elem1 = document.getElementsByClassName("navCenter2");
+        var elem2 = document.getElementsByClassName("navCenter");
+        if (isNavOpen) {
+            elem1[0].classList.replace("navCenter2", "navCenter");
+        }
+        else {
+            elem2[0].classList.replace("navCenter", "navCenter2");
+        }
+    }
+
+    
+    const navBar = (
         <>
         <div className='navBar'>
             <img className='logo' src={logo} alt="Logo" width="90" height="90"/>
             <nav className='navigation'>
+                <button className="btnToggleNav" onClick={() => toggleNav()}>Menu</button>
                 <NavLink className="current" to="/home"> Home </NavLink>
                 <NavLink className="current" to="/cocktails"> Cocktails </NavLink>
                 <NavLink className="current" to="/bars"> Bars </NavLink>
                 <NavLink className="current" to="/submit"> Submit your recipes </NavLink>
                 <NavLink className="current" to="/contact"> Contact </NavLink>
-                <button className='btnLogin-popup' onClick={() => logIn()}>Login</button>
+                {props.isLogged === false ? 
+                (
+                    <button className="btnLogin-popup" onClick={() => logIn()}>Login</button>
+                ) : (
+                    <button className="btnLogin-popup" onClick={() => logOut()}>Log out</button>
+                )}
+            </nav>
+            <nav className='navCenter2'>
+                <NavLink className="current" to="/home" onClick={() => toggleNav()}> Home </NavLink>
+                <NavLink className="current" to="/cocktails" onClick={() => toggleNav()}> Cocktails </NavLink>
+                <NavLink className="current" to="/bars" onClick={() => toggleNav()}> Bars </NavLink>
+                <NavLink className="current" to="/submit" onClick={() => toggleNav()}> Submit your recipes </NavLink>
+                <NavLink className="current" to="/contact" onClick={() => toggleNav()}> Contact </NavLink>
             </nav>
         </div>
         </>
     );
 
-    const navBarLogOut = (
-        <>
-        <div className='navBar'>
-            <img className='logo' src={logo} alt="Logo" width="90" height="90"/>
-            <nav className='navigation'>
-            <NavLink className="current" to="/home"> Home </NavLink>
-                <NavLink className="current" to="/cocktails"> Cocktails </NavLink>
-                <NavLink className="current" to="/bars"> Bars </NavLink>
-                <NavLink className="current" to="/submit"> Submit your recipes </NavLink>
-                <NavLink className="current" to="/contact"> Contact </NavLink>
-                <button className='btnLogin-popup' onClick={() => logOut()}>Log out</button>
-            </nav>
-        </div>
-        </>
-    );
-
-    if (props.isLogged === false) {
-        return navBarLogIn; 
-    }
-    else {
-        return navBarLogOut;
-    }
+    return navBar;
 }
