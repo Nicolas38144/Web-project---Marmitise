bf6ed5782f1154235592d4e8c70fc427d5f778ed
Nicolas Germani <nicolas.germani.ndv@gmail.com>
2023-05-22 06:03:00 +0200

    nouvel essai

--- api-marmitise/server.js
+++ api-marmitise/server.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     //origin: "http://localhost:3000"
-    origin: "https://marmitise.netlify.app/"
+    origin: "https://marmitise.netlify.app"
 }));
 
 
