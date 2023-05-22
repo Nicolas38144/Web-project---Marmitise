c7d26533867f02b8fa1f9dc9a2b500521e9f80c6
Nicolas Germani <nicolas.germani.ndv@gmail.com>
2023-05-22 06:14:11 +0200

    test

--- api-marmitise/server.js
+++ api-marmitise/server.js
@@ -39,8 +39,7 @@ app.use(bodyparser.json());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    //origin: "http://localhost:3000"
-    origin: "https://marmitise.netlify.app"
+    origin: "http://localhost:3000" || "https://marmitise.netlify.app"
 }));
 
 
